c9dd1a40dd5a36485ec2e3a7c5226c511e912a9e
vicky <vicky@akeo.no>
2020-03-16 14:08:25 +0530

    edit applicant model

--- core/services/skill.service.js
+++ core/services/skill.service.js
@@ -3,7 +3,10 @@ var Skills = require('../models/skills');
 module.exports.searchSkills =  async (req) => {
     let skip = 0, limit = 10;
     if (req.query.limit) limit = parseInt(req.query.limit);
-    return await Skills.find({ "name": { "$regex": req.query.search, "$options": "i" }, is_deleted: false }).skip(skip).limit(limit).exec();
+    if (req.query.offset) skip = parseInt(req.query.offset);
+    let count = await Skills.count({ "name": { "$regex": req.query.search, "$options": "i" }, is_deleted: false });
+    let skills = await Skills.find({ "name": { "$regex": req.query.search, "$options": "i" }, is_deleted: false }).skip(skip).limit(limit).exec();
+    return { count, skills };
 }
 
 module.exports.getAllByIds = async (skillIds) => {
