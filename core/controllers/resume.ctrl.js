f698eb370cb105c468206d11ead2152ce7ae5668
vicky <vicky@akeo.no>
2020-04-13 11:55:55 +0530

    variable name updated

--- core/controllers/resume.ctrl.js
+++ core/controllers/resume.ctrl.js
@@ -15,16 +15,16 @@ router.get("/:id",
             if (resume) {
                 filenamearray = resume.fileName.split(".");
                 if (resume && (filenamearray[1] == "docx" || filenamearray[1] == "doc")) {
-                    html_response = await applicantService.getHtmlForReumeBase64(resume.resume, filenamearray[1]);
+                    html_response = await resumeService.getHtmlForReumeBase64(resume.resume, filenamearray[1]);
                     resume.resume = filenamearray[1] == "doc" ? html_response.html.match(/<body[^>]*>[\s\S]*<\/body>/gi) : html_response.html;
                     file_path = html_response.destination;
                     html_file_path = html_response.html_file_path;
                 }
                 responseService.response(req, null, "Applicant resume", resume, res);
                 if (filenamearray[1] == "doc" || filenamearray[1] == "docx")
-                    applicantService.deleteFileFromLocal(file_path);
+                    resumeService.deleteFileFromLocal(file_path);
                 if (filenamearray[1] == "doc")
-                    applicantService.deleteFileFromLocal(html_file_path);
+                    resumeService.deleteFileFromLocal(html_file_path);
             } else {
                 let err = {
                     status: 200,
