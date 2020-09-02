15c0d30b22eb40e01ad53b1a5c7949aee4711105
yash <yash@akeo.no>
2020-09-02 08:54:28 +0200

    setup fixes: create file setup done after setup

--- deploy.sh
+++ deploy.sh
@@ -11,58 +11,62 @@ ez_docker_up(){
 }
 
 setup(){
-    # if [ ! -d elastic_search_data ]; then
-    #   mkdir elastic_search_data
-    # fi
+  setup_done_file=.setup_done
+  # if [ ! -d elastic_search_data ]; then
+  #   mkdir elastic_search_data
+  # fi
 
-    # if [ ! -d mongo_db ]; then
-    #   mkdir mongo_db
-    # fi
+  if [ -f "$setup_done_file" ]; then
+    echo "Setup already run once try to destroy it and then deploy again"
+    return
+  fi
 
-    db_up
+  db_up
 
-    echo "########## Wait few minutes, Setting MongoDb and Elastic Search Dockers #########################"
-    mongodb_status_check=`sudo docker logs ez_mongodb | grep "MongoDB init process complete;"`
-    # echo $mongodb_status_check
-    while [ -z "$mongodb_status_check" ]
-    do
-        sleep 40
-        echo "Waiting for mongodb docker to be running"
-        mongodb_status_check=`sudo docker logs ez_mongodb | grep "MongoDB init process complete;"`
-        # echo $mongodb_status_check
-    done
+  echo "########## Wait few minutes, Setting MongoDb and Elastic Search Dockers #########################"
+  mongodb_status_check=`sudo docker logs ez_mongodb | grep "MongoDB init process complete;"`
+  # echo $mongodb_status_check
+  while [ -z "$mongodb_status_check" ]
+  do
+      sleep 40
+      echo "Waiting for mongodb docker to be running"
+      mongodb_status_check=`sudo docker logs ez_mongodb | grep "MongoDB init process complete;"`
+      # echo $mongodb_status_check
+  done
 
-    echo "########## Wait few seconds, Setting core and engine services #######################"
-    sleep 10
+  echo "########## Wait few seconds, Setting core and engine services #######################"
+  sleep 10
 
-    ez_docker_up
+  ez_docker_up
 
-    admin_user_create_check=`sudo docker logs ez_web | grep "Admin Password:"`
-    #echo $admin_user_create_check
-    while [ -z "$admin_user_create_check" ]
-    do
-        sleep 1
-        echo "Waiting for admin user creation"
-        admin_user_create_check=`sudo docker logs ez_web | grep "Admin Password:"`
-        echo $admin_user_create_check > .adminpass
-        # echo $admin_user_create_check
-    done
-    ARCH=$(uname -s | grep Darwin)
-    if [ "$ARCH" == "Darwin" ]; then
-      privateIP=`ipconfig getifaddr en0`
-    else
-      privateIP=`ip route get 1 | awk '{print $NF;exit}'`
-    fi
+  admin_user_create_check=`sudo docker logs ez_web | grep "Admin Password:"`
+  #echo $admin_user_create_check
+  while [ -z "$admin_user_create_check" ]
+  do
+      sleep 1
+      echo "Waiting for admin user creation"
+      admin_user_create_check=`sudo docker logs ez_web | grep "Admin Password:"`
+      
+      # echo $admin_user_create_check
+  done
+  
+  ARCH=$(uname -s | grep Darwin)
+  if [ "$ARCH" == "Darwin" ]; then
+    privateIP=`ipconfig getifaddr en0`
+  else
+    privateIP=`ip route get 1 | awk '{print $NF;exit}'`
+  fi
 
-
-    echo "############## EazyRecruit has successfully setup: #########################################"
-    echo " "
-    echo "Access Url: http://${privateIP}/admin"
-    echo "            http://0.0.0.0/admin"
-    echo " "
-    echo "Admin User: admin@eazyrecruit.in"
-    echo " "
-    echo $admin_user_create_check
+  touch "$setup_done_file"
+  echo $admin_user_create_check > .adminpass
+  echo "############## EazyRecruit has successfully setup: #########################################"
+  echo " "
+  echo "Access Url: http://${privateIP}/admin"
+  echo "            http://0.0.0.0/admin"
+  echo " "
+  echo "Admin User: admin@eazyrecruit.in"
+  echo " "
+  echo $admin_user_create_check
 
 }
 
@@ -84,6 +88,7 @@ destroy(){
   sudo docker volume rm eazyrecruit_web_images
 
   sudo rm .adminpass
+  sudo rm .setup_done
   # sudo rm -r ./core/admin
   # if [ -d elastic_search_data ]; then
   #   cp -r elastic_search_data /tmp/eazyrecruit/
